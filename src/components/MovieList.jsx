46e0e844495e03385224cdb105a181b469f83dd2
arianoorqorbani <aria.noorghorbani@gmail.com>
2024-01-28 07:46:59 +0300

    Implement toggle lists

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function MovieList({ isOpen, setIsOpen, movies }) {
+  return (
+    <div className="box">
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
+      </button>
+      {isOpen && (
+        <ul className="list">
+          {movies?.map((movie) => (
+            <li key={movie.imdbID}>
+              <img src={movie.Poster} alt={`${movie.Title} poster`} />
+              <h3>{movie.Title}</h3>
+              <div>
+                <p>
+                  <span>🗓</span>
+                  <span>{movie.Year}</span>
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
